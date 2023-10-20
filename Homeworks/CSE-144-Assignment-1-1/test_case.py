83968087b477e0f43bbcf9c66f4e414843c376b1
MichaelXzj <45219543+MichaelXiongZJ@users.noreply.github.com>
2023-10-20 12:04:09 -0700

    add intercept

---
+++ Homeworks/CSE-144-Assignment-1-1/test_case.py
@@ -0,0 +1,12 @@
+from typing import List, Tuple
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import random
+
+
+A = np.array([1,2,3,4,5])
+B = np.array([2,2,2,2,2])
+# C = A.dot(B)
